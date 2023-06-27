4c6cf91010577918c6e21998890efa2eccc76ba3
abitoffka <aigerim.abitayevaa@gmail.com>
2023-06-27 13:26:34 +0600

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Aigerim Abitayeva on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
